bbf73868a9e4ddecaca14df9d6eb203a4de36068
Emil <rusev23@gmail.com>
2015-05-13 22:42:17 +0300

    test2

---
+++ test2/src/main/java/test_api2/test2/App.java
@@ -0,0 +1,13 @@
+package test_api2.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
