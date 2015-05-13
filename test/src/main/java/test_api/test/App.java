0b165cf51d393c1cf8ef403d2951899703d4b03f
Emil <rusev23@gmail.com>
2015-05-13 22:12:34 +0300

    test1

---
+++ test/src/main/java/test_api/test/App.java
@@ -0,0 +1,14 @@
+package test_api.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
